0a66c33241bd27df97205d4247fcabc669408bba
Saimanisuper <Saimanisuper@users.noreply.github.com>
2026-07-01 18:08:01 +0530

    Fix CORS issue with allow_credentials

--- api.py
+++ api.py
@@ -21,7 +21,7 @@ app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"], # Allow all origins for Vercel deployment
-    allow_credentials=True,
+    allow_credentials=False, # Must be False when origins is '*'
     allow_methods=["*"],
     allow_headers=["*"],
 )
